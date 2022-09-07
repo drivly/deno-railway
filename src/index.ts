b7629104ef68a15f1ed072a1d79e070b015def88
Nathan Clevenger <nateclev@gmail.com>
2022-09-07 15:43:55 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,9 @@
 
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts"
 
+ const headers = { 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Safari/605.1.15' }
  const port = parseInt(Deno.env.get("PORT") ?? "8000");
- serve(() => fetch('https://api.cf'),
+ serve(() => fetch('https://api.cf',{headers}),
        { addr: `:${port}` });
 
  console.log(`http://localhost:${port}/`);
