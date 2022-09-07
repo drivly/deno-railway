2ae083e4a894bc6235c6327256cd6b286d75199f
Nathan Clevenger <nateclev@gmail.com>
2022-09-07 04:44:56 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { serve } from "https://deno.land/std@0.145.0/http/server.ts"
 
 const port = parseInt(Deno.env.get("PORT") ?? "8000")
 serve((req: Request) => fetch('http://analytics.do/event'),
-       { addr: `:${port}` }))
+       { addr: `:${port}` })
